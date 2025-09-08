7be0da264171d8b9ea96f0f0d6eae58958ca7417
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2025-09-08 16:12:34 +0300

    add Vaildation

---
+++ src/Validation/index.ts
@@ -0,0 +1,40 @@
+
+
+
+interface IProduct {
+    title: string,
+    description: string,
+    image: string,
+    price: string,
+}
+
+
+export const ProductValidation = (product : IProduct ) => {
+
+    const errors: IProduct  = {
+        title: "",
+        description: "",
+        image: "",
+        price: ""
+    }
+
+    const validUrl = /^(ftp|http|https):\/\/[^ "]+$/.test(product.image)
+
+    if(!product.title.trim() || product.title.length < 10 || product.title.length > 80) {
+        errors.title = "Product title must be between 10 and 80 characters!"
+    }
+
+    if(!product.description.trim() || product.description.length < 10 || product.description.length > 900) {
+        errors.description = "Product description must be between 10 and 900 characters!"
+    }
+
+    if(!product.image.trim() || !validUrl) {
+        errors.image = "valid image URL is required!"
+    }
+
+    if(!product.price.trim() || isNaN(Number(product.price))) {
+        errors.price = "valid price is required!"
+    }
+
+    return errors
+}
---
+++ src/components/UiComponent/MsgError.tsx
@@ -0,0 +1,8 @@
+
+interface IProps {
+    msg: string
+}
+
+export const MsgError = ({msg} : IProps) => {
+  return msg ? <span className='block text-sm mt-1 text-red-500 font-semibold'>{msg}</span> : null
+}
